ef1e61769d04ca7ff67ac1148f2a64a620413b7a
danxmc <danx_97@hotmail.com>
2016-10-18 11:09:10 -0500

    Added comments

--- src/DataStructure/Stack/SStack.java
+++ src/DataStructure/Stack/SStack.java
@@ -17,10 +17,13 @@ class SStack {
     private int size = 0;
 
     //Methods
-    //Constrcutor
+    //Constructor
     public SStack() {
     }
 
+    /**
+     * Method que sirve para imprimir el Stack
+     */
     public void showStack() {
         for (int i = stack.length - 1; i >= 0; i--) {
             System.out.println("[" + stack[i] + "}");
@@ -30,24 +33,42 @@ class SStack {
         System.out.println("null\n");
     }
 
+    /**
+     * Method que inserta un Nodo al stack
+     * @param data, el contenido del nodo
+     */
     public void push(String data) {
         stack[size] = data;
         ++size;
     }
 
+    /**
+     * Method que hace pop(saca el dato que esta mas arriba del stack)
+     */
     public void pop() {
         stack[size - 1] = null;
         --size;
     }
 
+    /**
+     * 
+     * @return int size, el tamaño actual del stack   
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * 
+     * @return el dato en top del stack 
+     */
     public String getTop() {
         return stack[size - 1];
     }
     
+    /**
+     * Llena el stack con null
+     */
     public void eStack() {
         Arrays.fill(stack, null);
     }
